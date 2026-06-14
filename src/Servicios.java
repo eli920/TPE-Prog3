314bdd5a352d41460a00a52f3eb058eb9d363e00
Elisa Guaspari <eliguaspari@gmail.com>
2026-06-14 18:03:48 -0300

    Se agrega complejidad temporal a servicios 2 y 3

--- src/Servicios.java
+++ src/Servicios.java
@@ -14,7 +14,8 @@ public class Servicios {
     
     /*Expresar la complejidad temporal del constructor: O(n + m)
         donde n = cantidad de camiones y m = cantidad de paquetes. 
-        El CSV recorre una vez el archivo de camiones y una vez el archivo de paquetes*/
+        El CSV recorre una vez el archivo de camiones y una vez el archivo de paquetes
+    */
 
     public Servicios(String pathCamiones, String pathPaquetes){
         this.mapeoContAlimentos=new HashMap<>();
@@ -28,20 +29,33 @@ public class Servicios {
     }
 
     /*Expresar la complejidad temporal del servicio 1: O(1)
-      Al Hashmap se accede directamente por la clave(codigoPaquete) sin necesidad de recorrer nada*/
+      Al Hashmap se accede directamente por la clave(codigoPaquete) sin necesidad de recorrer nada
+    */
     public Paquete servicio1(String codigoPaquete) {
 
         return mapeoCodPaquete.get(codigoPaquete);
 
     }
 
-    /*Expresar la complejidad temporal del servicio 2.*/
+
+    /* Expresar la complejidad temporal del servicio 2: O(1)
+       Se utiliza un HashMap<Boolean, List<Paquete>>. La lista asociada a la clave true o false se obtiene mediante acceso directo,
+       sin recorrer estructuras adicionales.
+    */
     public List<Paquete> servicio2(boolean contieneAlimentos) {
         return mapeoContAlimentos.get(contieneAlimentos);
 
     }
 
-    /*Expresar la complejidad temporal del servicio 3.*/
+    /* Complejidad temporal: O(h + k)
+        Donde
+        h: altura del TreeMap utilizado para acceder al rango de urgencias.
+        k: cantidad de paquetes encontrados dentro de dicho rango.
+
+        Primero se obtiene el subrango de urgencias mediante subMap(), y luego se recorren únicamente los paquetes pertenecientes a ese rango.
+
+        En el peor caso, se devuelven todos los paquetes (k = n), por lo que la complejidad temporal resulta O(n).
+    */
     public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) {
         List<Paquete> resultado = new ArrayList<>();
         Map<Integer, List<Paquete>> subrango = indiceUrgencia.subMap(urgenciaMinima, true, urgenciaMaxima, true);
